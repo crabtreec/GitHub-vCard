7c05c3faacbec0b8b81e23a51ca6c84af7fe4832
Chad Crabtree <chadcrabtree@Chads-MacBook-Pro.local>
2017-08-10 15:58:50 -0400

    WIP GitHub vCard

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-const body = document.querySelector('body')
-const url = 'https://api.github.com/users/crabtreec';
-let vCard = [];
+let url = "https://api.github.com/users/crabtreec";
+let wrapper = document.querySelector(".body");
 
-function recieve(data) {
-  return data;
-}
+fetch(url)
+  .then(function(data){
+    return data.json();
+  })
+  .then(function(data){
+    let vCard =
+    `
+    <header>
+    <h1>${data.name}</h1>
+    </header>
+    <main>
+        <div>
+          <h2>The Basics</h2>
+            <ul>
+                <li><span>Name:</span> ${data.name}</li>
+                <li><span>GitHub URL:</span> <a href=${data.html_url}>${data.login}</a></li>
+                <li><span>Email:</span> ${data.email}</li>
+                <li><span>Company:</span> ${data.company}</li>
+                <li><span>Website:</span> <a href=${data.blog}>stephen-boynton.wordpress.com</a></li>
+            </ul>
+        </div>
+        <section id="right">
+          <h2>The Story</h2>
+              <p>${data.bio}</p>
+        </section>
+        <section>
+              <img src="${data.avatar_url}">
+        </section>
+    </main>
+    `;
+    wrapper.innerHTML = vCard;
 
-function transfer(data) {
-  return data.json();
-}
-
-function makevCard(data) {
-    vCard = data;
-  return vCard;
-}
-
-//====================================//
-
-function makeTitle(data) {
-  const div = document.createElement('div');
-  div.className = "title";
-  div.innerHTML = `<h1>${vCard.name}</h1>`;
-
-    body.appendChild(div);
-    return vCard;
-}
+})
